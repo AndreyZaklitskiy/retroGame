3cb981ed8611f3c9748bbff37e84aa7298f290b0
Andrey Zaklitskiy <31317623+AndreyZaklitskiy@users.noreply.github.com>
2024-04-13 13:02:02 +0300

    basic playable game

--- scripts.js
+++ scripts.js
@@ -1,20 +1,40 @@
 class Player {
     constructor(game) {
         this.game = game;
-        this.width = 100;
-        this.height = 100;
+        this.width = 140;
+        this.height = 120;
         this.x = this.game.width * 0.5 - this.width * 0.5;
         this.y = this.game.height - this.height;
-        this.speed = 10;
+        this.speed = 5;
         this.lives = 3;
+        this.maxLives = 10;
+        this.image = document.getElementById('player');
+        this.jets_image = document.getElementById('player_jets');
+        this.frameX = 0;
+        this.jetsFrame = 1;
     }
     draw(context) {
-        context.fillRect(this.x, this.y, this.width, this.height);
+        //handle sprite frames
+        if(this.game.keys.indexOf('1') > -1){
+            this.frameX = 1;
+        }else{
+            this.frameX = 0;
+        }
+        context.drawImage(this.jets_image, this.jetsFrame * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+
+        context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
     }
     update() {
         //horizontal movement
-        if(this.game.keys.indexOf('ArrowLeft') > -1) this.x -= this.speed;
-        if(this.game.keys.indexOf('ArrowRight') > -1) this.x += this.speed;
+        if(this.game.keys.indexOf('ArrowLeft') > -1){
+            this.x -= this.speed;
+            this.jetsFrame = 0;
+        } else if(this.game.keys.indexOf('ArrowRight') > -1){
+            this.x += this.speed;
+            this.jetsFrame = 2;
+        } else {
+            this.jetsFrame = 1;
+        }
         //horizontal boundaries
         if(this.x < -this.width * 0.5) this.x = -this.width * 0.5;
         else if(this.x > this.game.width - this.width * 0.5) this.x = this.game.width - this.width * 0.5;
@@ -32,8 +52,8 @@ class Player {
 
 class Projectile {
     constructor() {
-        this.width = 8;
-        this.height = 20;
+        this.width = 3;
+        this.height = 40;
         this.x = 0;
         this.y = 0;
         this.speed = 20;
@@ -41,7 +61,10 @@ class Projectile {
     }
     draw(context) {
         if(!this.free){
+            context.save();
+            context.fillStyle = 'gold';
             context.fillRect(this.x, this.y, this.width, this.height);
+            context.restore();
         }
     }
     update() {
@@ -93,16 +116,13 @@ class Enemy {
             }
         }
         // check collision enemies - player
-        if(this.game.checkCollision(this, this.game.player)){
-            this.markedForDeletion = true;
-            if(!this.game.gameOver && this.game.score>0) this.game.score--;
+        if(this.game.checkCollision(this, this.game.player) && this.lives > 0){
+            this.lives = 0;
             this.game.player.lives--;
-            if(this.game.player.lives < 1) this.game.gameOver = true;
         }
         // lose condition
-        if(this.y + this.height > this.game.height){
+        if(this.y + this.height > this.game.height || this.game.player.lives < 1){
             this.game.gameOver = true;
-            this.markedForDeletion = true;
         }
     }
     hit(damage){
@@ -170,7 +190,7 @@ class Game {
         this.player = new Player(this);
 
         this.ProjectilesPool = [];
-        this.numberOfProjectiles = 10;
+        this.numberOfProjectiles = 15;
         this.createProjectiles();
         this.fired = false;
         
@@ -184,7 +204,7 @@ class Game {
 
         this.spriteUpdate = false;
         this.spriteTimer = 0;
-        this.spriteInterval = 120;
+        this.spriteInterval = 150;
 
         this.score = 0;
         this.gameOver = false;
@@ -201,7 +221,7 @@ class Game {
             const index = this.keys.indexOf(e.key);
             if(index > -1) this.keys.splice(index, 1)
         })
-    }
+    } 
     render(context, deltaTime) {
         //sprite timing
         if(this.spriteTimer > this.spriteInterval){
@@ -213,19 +233,19 @@ class Game {
         }
 
         this.drawStatusText(context);
-        this.player.draw(context);
-        this.player.update();
         this.ProjectilesPool.forEach(projectile => {
             projectile.update();
             projectile.draw(context);
         })
+        this.player.draw(context);
+        this.player.update();
         this.waves.forEach(wave => {
             wave.render(context);
             if(wave.enemies.length < 1 && !wave.nextWaveTrigger && !this.gameOver){
                 this.newWave();
                 this.waveCount++;
                 wave.nextWaveTrigger = true;
-                this.player.lives++;
+                if(this.player.lives < this.player.maxLives) this.player.lives++;
             }
         })
     }
@@ -257,8 +277,11 @@ class Game {
         context.shadowColor = 'black';
         context.fillText(`Score: ${this.score}`, 20, 40);
         context.fillText(`Wave: ${this.waveCount}`, 20, 80);
+        for(let i = 0; i < this.player.maxLives; i++) {
+            context.strokeRect(20 + 20*i, 100, 10, 15);
+        }
         for(let i = 0; i < this.player.lives; i++) {
-            context.fillRect(20 + 10*i, 100, 5, 20);
+            context.fillRect(20 + 20*i, 100, 10, 15);
         }
         if(this.gameOver){   
             context.textAlign = 'center';
@@ -297,7 +320,6 @@ window.addEventListener('load', function() {
     canvas.height = 800;
     ctx.fillStyle = 'white';
     ctx.strokeStyle = 'white';
-    ctx.lineWidth = 5;
     ctx.font = '30px Impact';
 
     const game = new Game(canvas);
